5b970223716f39c0a99e621c72be881dc3c4a421
0xfzz <138555744+0xfzz@users.noreply.github.com>
2023-12-27 21:24:03 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,6 +147,11 @@ const all_content = document.querySelectorAll(".content");
 const line = document.querySelector(".line");
 
 tabs.forEach((tab, index) => {
+    const isActive = tab.classList.contains("active")
+    if(isActive){
+        line.style.width = tab.offsetWidth + "px";
+        line.style.left = tab.offsetLeft + "px";
+    }
     tab.addEventListener("click", (e) => {
         tabs.forEach(tab => { tab.classList.remove("active") });
         tab.classList.add("active");
