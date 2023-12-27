208a7a355e9a5d896925a444bc8975bc5c70d5e7
EKZETA <ekopriambodo12gt@gmail.com>
2023-12-27 11:01:56 +0700

    perubahan

--- assets/js/main.js
+++ assets/js/main.js
@@ -142,3 +142,19 @@ document.addEventListener("scroll", (e) => {
         navbar.classList.remove("bg-black")
     }
 })
+const tabs = document.querySelectorAll(".tab_btn");
+const all_content = document.querySelectorAll(".content");
+const line = document.querySelector(".line");
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener("click", (e) => {
+        tabs.forEach(tab => { tab.classList.remove("active") });
+        tab.classList.add("active");
+
+        line.style.width = e.target.offsetWidth + "px";
+        line.style.left = e.target.offsetLeft + "px";
+
+        all_content.forEach(content => { content.classList.remove("active") });
+        all_content[index].classList.add("active");
+    })
+})
